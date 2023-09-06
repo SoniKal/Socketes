9ef65183eccf37c00ddfc9eb86eb0255186491c6
TheChosenOne7373836 <tadeods2005@gmail.com>
2023-09-06 15:20:16 -0300

    V7-Testing

--- SocketsTCP/src/AES.java
+++ SocketsTCP/src/AES.java
@@ -7,31 +7,30 @@ import java.util.Base64;
 
 public class AES {
     public static void main(String[] args) throws Exception {
-        // La clave debe tener 16, 24 o 32 bytes para AES-128, AES-192 o AES-256, respectivamente
-        String clave = "MiClaveSecreta123"; // Cambia esto por tu clave secreta
-        String textoOriginal = "Hola, este es mi texto original.";
+        // clave debe tener 16, 24 o 32 bytes para AES
+        String clave = "dummytest"; // Cambia esto por tu clave secreta
+        String textoOriginal = "hello world";
 
-        // Genera un vector de inicialización (IV)
         byte[] iv = new byte[16];
         IvParameterSpec ivSpec = new IvParameterSpec(iv);
 
-        // Crea una clave secreta a partir de la clave proporcionada
+        // crea clave
         SecretKeyFactory factory = SecretKeyFactory.getInstance("AES");
         SecretKey key = new SecretKeySpec(clave.getBytes(), "AES");
 
-        // Inicializa el cifrado
+        // inicia cifrado
         Cipher cifrador = Cipher.getInstance("AES/CBC/PKCS5Padding");
         cifrador.init(Cipher.ENCRYPT_MODE, key, ivSpec);
 
-        // Cifra el texto original
+        // texto original cifrado
         byte[] textoCifrado = cifrador.doFinal(textoOriginal.getBytes("UTF-8"));
 
-        // Convierte el resultado a una representación en base64
+        // convierte el resultado a base64
         String textoCifradoBase64 = Base64.getEncoder().encodeToString(textoCifrado);
 
         System.out.println("Texto cifrado: " + textoCifradoBase64);
 
-        // Puedes descifrar el texto cifrado de la siguiente manera
+        // descifra el texto cifrado
         cifrador.init(Cipher.DECRYPT_MODE, key, ivSpec);
         byte[] textoDescifrado = cifrador.doFinal(Base64.getDecoder().decode(textoCifradoBase64));
         String textoOriginalDescifrado = new String(textoDescifrado, "UTF-8");
