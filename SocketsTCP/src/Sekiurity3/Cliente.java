547c6f4706e6d7bc2a90c5dfb1ad38b1e1cd01b5
fabricio_fiesta <tiago.ruckj@gmail.com>
2023-09-27 10:51:53 -0300

    huberto

--- SocketsTCP/src/Sekiurity3/Cliente.java
+++ SocketsTCP/src/Sekiurity3/Cliente.java
@@ -1,5 +1,8 @@
 package Sekiurity3;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 import java.io.*;
 import java.net.*;
 
@@ -8,52 +11,78 @@ public class Cliente {
     private static final int PUERTO = 6969;
 
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new ChatClientFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
+
+class ChatClientFrame extends JFrame {
+    private static final String SERVIDOR_IP = "172.16.255.221"; // Cambiar a la IP del servidor si es necesario
+    private static final int PUERTO = 6969;
+
+    private Socket socket;
+    private ObjectOutputStream out;
+
+    private JTextField messageField;
+    private JTextArea chatArea;
+
+    public ChatClientFrame() {
+        setTitle("Cliente de Chat");
+        setSize(400, 300);
+        setLayout(new BorderLayout());
+
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(chatArea);
+        add(scrollPane, BorderLayout.CENTER);
+
+        messageField = new JTextField();
+        messageField.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                sendMessage(messageField.getText());
+            }
+        });
+        add(messageField, BorderLayout.SOUTH);
+
         try {
-            Socket socket = new Socket(SERVIDOR_IP, PUERTO);
+            socket = new Socket(SERVIDOR_IP, PUERTO);
+            out = new ObjectOutputStream(socket.getOutputStream());
 
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-
-            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-
-            System.out.println("Conectado al servidor. Escribe un mensaje o 'salir' para desconectarte.");
-
-            // Hilo para recibir y mostrar mensajes del servidor
-            Thread recibirMensajes = new Thread(() -> {
-                try {
-                    while (true) {
-                        // Recibe un objeto Mensaje del servidor y lo muestra
-                        Mensaje mensajeRecibido = (Mensaje) in.readObject();
-                        System.out.println(mensajeRecibido);
+            Thread recibirMensajes = new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+                        while (true) {
+                            Mensaje mensajeRecibido = (Mensaje) in.readObject();
+                            chatArea.append(mensajeRecibido.toString() + "\n");
+                        }
+                    } catch (IOException | ClassNotFoundException e) {
+                        e.printStackTrace();
                     }
-                } catch (IOException | ClassNotFoundException e) {
-                    e.printStackTrace();
                 }
             });
             recibirMensajes.start();
 
-            // Hilo principal para enviar mensajes al servidor
-            String userInput;
-            while ((userInput = stdin.readLine()) != null) {
-                // Crear un objeto Mensaje con el texto ingresado
-                Mensaje mensaje = new Mensaje(socket.getLocalAddress().getHostAddress(), userInput);
-                out.writeObject(mensaje);
-                out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-                if (userInput.equalsIgnoreCase("salir")) {
-                    break;
-                }
-            }
-
-            out.close();
-            in.close();
-            stdin.close();
-            socket.close();
+    private void sendMessage(String message) {
+        try {
+            Mensaje mensaje = new Mensaje(socket.getLocalAddress().getHostAddress(), message);
+            out.writeObject(mensaje);
+            out.flush();
+            messageField.setText("");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
 
 
 
