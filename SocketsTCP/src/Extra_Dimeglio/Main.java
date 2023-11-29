1443b16aa61db521e989d5038504b9d9ca2a27d0
TheChosenOne7373836 <tadeods2005@gmail.com>
2023-11-29 16:28:40 -0300

    TCP_DimV6

--- SocketsTCP/src/Extra_Dimeglio/Main.java
+++ SocketsTCP/src/Extra_Dimeglio/Main.java
@@ -1,4 +1,5 @@
 package Extra_Dimeglio;
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
@@ -8,7 +9,6 @@ public class Main {
         Usuario usuario = new Usuario();
         String publica = Usuario.interfazIP();
         Scanner scanner = new Scanner(System.in);
-
 
         if (publica != null) {
             System.out.println("----------------------------------------------------------");
@@ -32,14 +32,13 @@ public class Main {
 
         if (posicion != -1) {
             System.out.println("----------------------------------------------------------");
-            System.out.println("Usuario: " + usuario.getUsuarios().get(posicion-1).getNombreUsuario());
-            System.out.println("Dir. IP: " + usuario.getUsuarios().get(posicion-1).getDireccionIP());
-            System.out.println("Puerto: " + usuario.getUsuarios().get(posicion-1).getPuertoUsuario());
-            System.out.println("Posicion: [" + posicion+"]");
+            System.out.println("Usuario: " + usuario.getUsuarios().get(posicion - 1).getNombreUsuario());
+            System.out.println("Dir. IP: " + usuario.getUsuarios().get(posicion - 1).getDireccionIP());
+            System.out.println("Puerto: " + usuario.getUsuarios().get(posicion - 1).getPuertoUsuario());
+            System.out.println("Posicion: [" + posicion + "]");
             System.out.println("----------------------------------------------------------");
             System.out.println("Topologia: ");
-            for (Usuario z : usuario.getUsuarios())
-            {
+            for (Usuario z : usuario.getUsuarios()) {
                 System.out.println(z.getNombreUsuario() + ": " + z.getDireccionIP());
             }
             System.out.println("----------------------------------------------------------");
@@ -51,7 +50,7 @@ public class Main {
             return;
         }
 
-        new Thread(() -> {  //usuario puede recibir mensajes
+        new Thread(() -> {  // usuario puede recibir mensajes
             try {
                 ServerSocket serverSocket = new ServerSocket(12345);
                 System.out.println("Esperando conexiones    .   .   .");
@@ -60,33 +59,35 @@ public class Main {
                     Socket clienteSocket = serverSocket.accept();
                     System.out.println("Cliente conectado.");
 
-                    ObjectInputStream inputStream = new ObjectInputStream(clienteSocket.getInputStream());
-
                     new Thread(() -> {
                         try {
                             while (true) {
+                                ObjectInputStream inputStream = new ObjectInputStream(clienteSocket.getInputStream());
                                 Mensaje mensajeRecibido = (Mensaje) inputStream.readObject();
-                                for (Usuario u: usuario.getUsuarios())
-                                {
-                                    if (u.getDireccionIP().equals(publica)){
+                                for (Usuario u : usuario.getUsuarios()) {
+                                    if (u.getDireccionIP().equals(publica)) {
                                         u.recibir(mensajeRecibido);
                                     }
                                 }
                             }
-                        } catch (IOException | ClassNotFoundException ignored) {}
+                        } catch (IOException | ClassNotFoundException ignored) {
+                        }
                     }).start();
                 }
             } catch (IOException ignored) {
             }
         }).start();
 
-        while (true) {  //usuario puede enviar mensajes
+        while (true) {  // usuario puede enviar mensajes
             String usuarioDest = scanner.nextLine();
+            if (usuarioDest.equalsIgnoreCase("exit")) {
+                break;  // Salir del bucle al ingresar "exit"
+            }
             String mensajeDest = scanner.nextLine();
 
             Mensaje mensaje = new Mensaje(mensajeDest, usuarioDest, usuario.getNombreUsuario());
 
-            for (Usuario uV2: usuario.getUsuarios()) {
+            for (Usuario uV2 : usuario.getUsuarios()) {
                 if (Objects.equals(uV2.getDireccionIP(), publica)) {
                     uV2.enviar(mensaje);
                 }
